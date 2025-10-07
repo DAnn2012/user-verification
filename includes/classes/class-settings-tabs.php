41e88e95ff7c85bf8fa4993143912a8ed1390011
DAnn2012 <dannunziata@hotmail.it>
2025-10-07 12:13:48 +0200

    Update class-settings-tabs.php

--- includes/classes/class-settings-tabs.php
+++ includes/classes/class-settings-tabs.php
@@ -1735,7 +1735,7 @@ if (!class_exists('settings_tabs_field')) {
 
             ob_start();
         ?>
-            <input colorPicker="" name="<?php echo esc_attr($field_name); ?>" id="<?php echo esc_attr($css_id); ?>" placeholder="<?php echo esc_attr(esc_attr($placeholder)); ?>" value="<?php echo esc_attr($value); ?>" />
+            <input colorPicker="" name="<?php echo esc_attr($field_name); ?>" id="<?php echo esc_attr($css_id); ?>" placeholder="<?php echo esc_attr($placeholder); ?>" value="<?php echo esc_attr($value); ?>" />
             <?php
 
             $input_html = ob_get_clean();
